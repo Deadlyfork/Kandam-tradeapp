0d1f5949759a318a34b8b5b838e3204411afd5c9
Deadlyfork <89066793+Deadlyfork@users.noreply.github.com>
2021-08-20 15:45:36 +0530

    Squashed commit of the following:
    commit ffaa3ae98af9d18ce2b8760e59295b2d7d770e20
    Author: Deadlyfork <89066793+Deadlyfork@users.noreply.github.com>
    Date:   Fri Aug 20 15:43:05 2021 +0530
    
        Update kandam.py
    
    commit 49cb25d81c0753f758d07a59502988f097593c44
    Author: Deadlyfork <89066793+Deadlyfork@users.noreply.github.com>
    Date:   Fri Aug 20 15:17:46 2021 +0530
    
        Revert "Revert "Update kandam.py""
    
        This reverts commit e1bd968b3c9673db8c84e020a73bb9abd41ce967.
    
    commit e1bd968b3c9673db8c84e020a73bb9abd41ce967
    Author: Deadlyfork <89066793+Deadlyfork@users.noreply.github.com>
    Date:   Fri Aug 20 15:16:24 2021 +0530
    
        Revert "Update kandam.py"
    
        This reverts commit 9d9988e87f6cdfbc55720ee1f2a63aad80b75302.
    
    commit 9d9988e87f6cdfbc55720ee1f2a63aad80b75302
    Author: Deadlyfork <89066793+Deadlyfork@users.noreply.github.com>
    Date:   Fri Aug 20 15:14:07 2021 +0530
    
        Update kandam.py
    
    commit 95c4d7422a99761a510182e8ae794ae364d68f6f
    Author: Deadlyfork <89066793+Deadlyfork@users.noreply.github.com>
    Date:   Fri Aug 20 13:48:45 2021 +0530
    
        Update kandam.py

--- kandam.py
+++ kandam.py
@@ -8,31 +8,87 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+class Menu(GridLayout):
+    #Screen 3
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rows = 2
+        
+
+class InfoPage(GridLayout):
+    # SCREEN 2
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+        
+        self.message = Label(halign='center', valign= 'middle', font_size=30)
+        self.message.bind(width=self.update_text_width)
+        self.add_widget(self.message)
+
+    def update_info(self, message):
+        self.message.text = message
+
+    def update_text_width(self, *_):
+        #For the message not to go beyond the window
+        self.message.text_size = (self.message.width*0.9, None)
+
+
+
 class LoginScreen(GridLayout):
-    # LOGIN SCREEN
+    # LOGIN SCREEN / SCREEN 1
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cols = 2
         
+        #Inputing email
         self.add_widget(Label(text='Email'))
         self.email = TextInput(multiline= False)
         self.add_widget(self.email)
         
+        #Inputing password
         self.add_widget(Label(text= 'Password'))
         self.password = TextInput(multiline= False)
         self.add_widget(self.password)
         
+        #Building a submit button
         self.submit = Button(text='Submit')
+        self.submit.bind(on_press=self.submit_button)
         self.add_widget(self.submit)
 
-    def submit_button(self):
+    def submit_button(self, instance):
+        #After submition
         email = self.email.text
-        password = self.password.text        
+        password = self.password.text
+        info = ';)' 
+        trade_app.info_page.update_info(info)       
+        trade_app.screen_manager.current = 'info'
 
 class kandam(App):
     # BASE CLASS
     def build(self):
-        return LoginScreen()
+        #Building a window to display 
+        self.screen_manager = ScreenManager()
+        self.login_screen = LoginScreen()
+        
+        #Building screen 1 - Login screen
+        screen = Screen()
+        screen.add_widget(self.login_screen)
+        self.screen_manager.add_widget(screen)
+        
+        #Building screen 2 - information page
+        self.info_page = InfoPage()
+        screen = Screen(name='info')
+        screen.add_widget(self.info_page)
+        self.screen_manager.add_widget(screen)
+
+        #Building screen 3 - Menu
+        self.menu = Menu()
+        screen = Screen(name ='menu')
+        screen.add_widget(self.menu)
+        self.screen_manager.add_widget(screen)
+
+        return self.screen_manager
 
 if __name__ == '__main__':
-    kandam().run()
+    trade_app = kandam()
+    trade_app.run()
