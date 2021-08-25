f5b05d9ef7111fde81a19f170654f0635257ed9b
Deadlyfork <89066793+Deadlyfork@users.noreply.github.com>
2021-08-25 09:06:50 +0530

    Update kandam.py

--- kandam.py
+++ kandam.py
@@ -1,14 +1,35 @@
-from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.screen import MDScreen
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+'''
+class SearchPage(MDScreen):
+    pass
 
-kv_file = Builder.load_file('kandam.kv')
+class NewTradePage(MDScreen):
+    pass
 
+class MyTradesPage(MDScreen):
+    pass
+
+class HomePage(MDScreen):
+    pass
+
+class RegisterPage(MDScreen):
+    pass
+'''
+class LoginPage(MDScreen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+kv_file = Builder.load_file('kandam_builder.kv')
 
 class Kandam(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark" 
-        return kv_file()
+        return kv_file
 
 if __name__ == '__main__':
     Kandam().run()
