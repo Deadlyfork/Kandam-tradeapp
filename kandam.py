0f761b22f31617c8988ca4ddf8c0d675116fda98
Deadlyfork <89066793+Deadlyfork@users.noreply.github.com>
2021-08-25 17:53:06 +0530

    Update kandam.py

--- kandam.py
+++ kandam.py
@@ -1,12 +1,13 @@
-from kivymd.app import MDApp
+
 from kivy.lang import Builder
 
+from kivymd.app import MDApp
 
-kv_file = Builder.load_file("kandam_builder.kv")
+#kv_file = Builder.load_file("kandam_builder.kv")
 
 class Kandam(MDApp):
     def build(self):
-        return kv_file
+        return Builder.load_file("kandam_builder.kv")
 
 if __name__ == '__main__':
     Kandam().run()
