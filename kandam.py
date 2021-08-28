b7b4f7056fd23cf8a11c47d7a4572a461ba99bbe
Deadlyfork <89066793+Deadlyfork@users.noreply.github.com>
2021-08-28 16:57:01 +0530

    yeet

--- kandam.py
+++ kandam.py
@@ -2,6 +2,10 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
+from kivymd.icon_definitions import md_icons
+
+class SearchPage(MDScreen):
+    pass
 
 class HomePage(MDScreen):
     pass
@@ -18,6 +22,7 @@ class ScreenManagement(ScreenManager):
 class Kandam(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = 'BlueGray'
         return Builder.load_file("kandam_builder.kv")
 
 if __name__ == '__main__':
