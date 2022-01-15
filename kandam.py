727f18acc581bf24c85925fe494b62a0d2f72dd5
RoMaNTii <89082916+RoMaNTii@users.noreply.github.com>
2022-01-15 15:24:21 +0530

    Update kandam.py

--- kandam.py
+++ kandam.py
@@ -1,4 +1,15 @@
 from kivy.lang import Builder
+=======
+
+import kivy
+kivy.require('2.0.0')
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+>>>>>>> Stashed changes
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
